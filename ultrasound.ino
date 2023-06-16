f62b1e41ccd8faf77af93b4b7d0f6ddc1c6c56db
AlexPitoiset <118912145+AlexPitoiset@users.noreply.github.com>
2023-06-16 09:07:41 +0200

    ultrasound code

--- ultrasound.ino
+++ ultrasound.ino
@@ -1,35 +1,32 @@
 const int pingPin = 9;
-void setup() {
-Serial.begin(9600);
 
-}
+long ultrasound_read() {
+  long duration, inches, cm;
 
-void loop() {
-long duration, inches, cm;
-pinMode(pingPin,OUTPUT);
-digitalWrite(pingPin,LOW);
-delayMicroseconds(2);
-digitalWrite(pingPin,HIGH);
-delayMicroseconds(5);
-digitalWrite(pingPin,LOW);
+  pinMode(pingPin,OUTPUT);
+  digitalWrite(pingPin,LOW);
+  delayMicroseconds(2);
+  digitalWrite(pingPin,HIGH);
+  delayMicroseconds(5);
+  digitalWrite(pingPin,LOW);
 
+  pinMode(pingPin,INPUT);
+  duration = pulseIn(pingPin,HIGH);
+  cm = microsecondsToCentimeters(duration);
 
-pinMode(pingPin,INPUT);
-duration = pulseIn(pingPin,HIGH);
-cm = microsecondsToCentimeters(duration);
-Serial.print(cm);
-Serial.print("cm");
-int i = (cm/313)*10;
-for (int z=0;z<i;z++) {
-  Serial.println("-");
-}
-i = 0;
-Serial.println();
-delay(10);
+  return cm;
 
+  /*
+  Serial.print(cm);
+  Serial.print("cm");
+  int i = (cm/313)*10;
+  for (int z=0;z<i;z++) {
+    Serial.println("-");
+  }
+  i = 0;
+  */
 }
 
 long microsecondsToCentimeters(long microseconds){
   return microseconds/29/2;
-  
-  }
+}
